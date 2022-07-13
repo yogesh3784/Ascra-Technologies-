400427e145602e5a676e466974f0de9d9b657d30
yogesh3784 <109205223+yogesh3784@users.noreply.github.com>
2022-07-13 17:39:36 +0530

    Update task5.py

---
+++ task5.py
@@ -0,0 +1,4 @@
+There is no NULL data type. 
+Unlike nil is treated as false in the Boolean contexts, the null has no further meaning. 
+We should not be referring null unless the exercise's original intent was to detect :null or Null atom and remove them from the list. 
+Otherwise, it might be confusing.
