817a312f57e71d4e65201bd8f455073dc3fe4450
Yogesh Pal <109205223+yogesh3784@users.noreply.github.com>
2022-07-18 13:52:29 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-def sum_square_difference(n):
-    numbers = range(1, n+1)
-    sum_squares = sum(i**2 for i in numbers)
-    square_sum = sum(numbers)**2
-    return square_sum - sum_squares
-
-print(sum_square_difference(100))
