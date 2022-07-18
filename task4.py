c7aa33b897e1a7cfaa33e1385b9c17d2065cc2d4
Yogesh Pal <109205223+yogesh3784@users.noreply.github.com>
2022-07-18 13:52:38 +0530

    Delete task4.py

--- task4.py
+++
@@ -1,7 +0,0 @@
-def sum_of_multiples(limit, values):
-    multiples = set()
-    for val in values:
-        if val > 0:
-            for multiple in range(val,limit,val):
-                multiples.add(multiple)
-    return sum(multiples)
